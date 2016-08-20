d1f22b1b0ea3d21bde302900692c41c0f223236e
irwindegannes <irwin.degannes@gmail.com>
2016-08-20 04:20:39 -0400

    first commit - added bootstrap functionality, customized site.master, started home page layout.

---
+++ EADD_Final_Project/Home.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Title="" Language="vb" AutoEventWireup="false" MasterPageFile="~/Site.Master" CodeBehind="Home.aspx.vb" Inherits="EADD_Final_Project.Home" %>
+<asp:Content ID="HeadContent" ContentPlaceHolderID="head" runat="server">
+        
+    <style type="text/css">
+        .auto-style1 {
+            text-decoration: underline;
+        }
+    </style>
+        
+</asp:Content>
+<asp:Content ID="BodyContent" ContentPlaceHolderID="content" runat="server">
+    
+    <div class="container-fluid" style="margin-top:60px">
+        <div class="row">
+            <div class="col-md-9" style="border-style:groove">
+                <div class="col-md-3">
+                    <asp:Image ID="ProfilePicture" runat="server" AlternateText="Profile Picture" ImageUrl="assets/profilepicture.png" Height="160px" Width="100%" />
+                        <br />
+                    <asp:Label ID="Rank" runat="server" Text="Rank: "></asp:Label>
+                    
+                </div>
+                <div class="col-md-6" style="border-style:groove">
+                    test
+                </div>
+            </div>
+            <div class="col-md-3" style="border-style:groove">
+                <h2 style="text-align:center">Recent Activity</h2><br /><br />
+            </div>
+        
+        </div>
+    </div>
+
+
+
+</asp:Content>
---
+++ EADD_Final_Project/js/myJavaScript.js
@@ -0,0 +1,35 @@
+﻿$(function() {
+
+    ////to set top active navbar selection
+    //$("#home a:contains('Home')").parent().addClass('active');
+    //$("#lessons a:contains('Lessons')").parent().addClass('active');
+    //$("#progress a:contains('Progress')").parent().addClass('active');
+    //$("#achievements a:contains('Achievements')").parent().addClass('active');
+
+    ////to set botton active navbar selection
+    //$("#account a:contains('Account')").parent().addClass('active');
+    //$("#about a:contains('About')").parent().addClass('active');
+    //$("#help a:contains('Help')").parent().addClass('active');
+    //$("#contact a:contains('Contact')").parent().addClass('active');
+
+    var str = location.href.toLowerCase();
+    $("#nav li a").each(function () {
+        if (str.indexOf($(this).attr("href").toLowerCase()) > -1) {
+            $("li.current-menu-item").removeClass("current-menu-item");
+            $(this).parent().addClass("current-menu-item");
+        }
+    });
+    $("li.current-menu-item").parents().each(function () {
+        if ($(this).is("li")) {
+            $(this).addClass("current-menu-item");
+        }
+    });
+
+    //to expand menu when hovering over dropdown item
+    $('ul.nav li.dropdown').hover(function () {
+        $('dropdown-menu', this).fadeIn();
+    }, function () {
+        $('.dropdown-menu', this).fadeOut('fast');
+    });
+
+});
